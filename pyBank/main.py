f70b07063bdc86c7bbd24ced26da353c9c7a2834
Christina Vavoulis <christinavavoulis@gmail.com>
2018-11-05 20:43:56 -0800

    updated analysis using pandas

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,51 +1,41 @@
 import os
 import csv
+import pandas as pd
+import numpy as np
 
-months=[]
-revenue=[]
-changeinpl=0
-all_changes=[]
-avg_change=0
-max_increase=0
-max_decrease=0
+csvpath=("budget_data.csv")
 
-csvpath=os.path.join("..","pyBank","budget_data.csv")
-with open (csvpath, 'r') as csvfile: 
-    csvreader=csv.reader(csvfile, delimiter=",")
+budget_df=pd.read_csv(csvpath)
+budget_df.head()
 
+total_months=budget_df["Date"].count()
+sum_p_l=budget_df["Profit/Losses"].sum()
 
-    header=next(csvreader)
+AmtChange = budget_df["Profit/Losses"].diff()
+budget_df["Amount Changed"] = AmtChange
 
+sum_change=budget_df["Amount Changed"].sum()
+sum_months=total_months-1
+avg_change=sum_change/sum_months
+budget_df.head()
 
-    for row in csvreader:
-        months.append(row[0])
-        revenue.append(int(row[1]))
-        totalrevenue=sum(revenue)
-        totalmonths=len(months)
+greatest_increase=budget_df["Amount Changed"].max()
 
+greatest_decrease=budget_df["Amount Changed"].min()
 
-for row in range(totalmonths-1): 
-    changeinpl=revenue[row+1]-revenue[row]
-    all_changes=all_changes + [changeinpl]
+greatest_dec_month=budget_df.loc[budget_df["Amount Changed"]==greatest_decrease,["Date"]]
+great_dec=greatest_dec_month["Date"].values[0]
 
+greatest_inc_month=budget_df.loc[budget_df["Amount Changed"]==greatest_increase,["Date"]]
+great_inc=greatest_inc_month["Date"].values[0]
 
-avg_change = sum(all_changes)/(totalmonths-1)
-max_increase=(max(all_changes))
-max_index=all_changes.index(max_increase)
-max_month=months[max_index+1]
+output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${sum_p_l}\n"
+    f"Average  Change: ${avg_change}\n"
+    f"Greatest Increase in Profits: {great_inc} (${greatest_increase})\n"
+    f"Greatest Decrease in Profits: {great_dec} (${greatest_decrease})\n")
 
-max_decrease=(min(all_changes))
-min_index=all_changes.index(max_decrease)
-min_month=months[min_index+1]
-
-# max_decrease=changeinpl.index(min(all_changes))
-
-
-print("Financial Analysis")
-print("-----------------------")
-print("Total Months: "+ str(totalmonths))
-print("Total: "+ str(totalrevenue))
-print("Average change: "+ str(avg_change))
-print("Greatest Increase in Profits "+ str(max_month) + " " +str(max_increase))
-print("Greatest Decrease in Profits " + str(min_month) + " " +str(max_decrease))
-
+print(output)    
